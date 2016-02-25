a02fd376bc2210e9ec38e8d28b28a8b0173d321c
xiaod <xiaod@leosys.com.cn>
2016-02-25 16:31:43 +0800

    用户登入验证

---
+++ js/token.js
@@ -0,0 +1,17 @@
+ function yanzheng(token){
+
+if(token==null||token==""){
+  window.location.href="./login.html";
+}
+
+}
+ yanzheng(window.sessionStorage.getItem("token"));
+
+function lost(){
+	window.sessionStorage.setItem("token","");
+	yanzheng(window.sessionStorage.getItem("token"));
+	
+}
+setTimeout("lost()",10000);
+
+
